872f77a1360a26dae4277b7a4ed50ce159f8ab5f
Oscar Vazquez <0skr.vazquez@gmail.com>
2024-05-19 20:11:20 -0600

    sigue flujo

--- src/flujo_control.rs
+++ src/flujo_control.rs
@@ -10,4 +10,25 @@ fn main() {
     } else {
         println!("el numero no es divisible ni por 4, 3, o 2");
     }
+    
+    let condicion = true;
+    let numero = if condicion { 5 } else { 6 };
+    
+    // Esto no compila por que los valores que podria devolver el if 
+    // no son el mismo uno es un i32 y el otro es un string
+    // let numero = if condicion { 3 } else { "seis" };
+
+    println!("El valor de numero es: {numero}");
+    
+    let mut contador = 0;
+
+    let resultado = loop {
+        contador += 1;
+
+        if contador == 10 {
+            break 100;
+        }
+    };
+
+    println!("El resultado de la iteracion es {resultado}");
 }
