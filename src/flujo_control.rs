6513c598bed1a9365c5e4d7dcd716a028aa2aec7
Oscar Vazquez <0skr.vazquez@gmail.com>
2024-05-19 19:27:25 -0600

    agregando mas flujo

--- src/flujo_control.rs
+++ src/flujo_control.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let numero = 7;
+    let numero = 6;
 
-    if numero < 5 {
-        println!("La condicion es verdadera!");
+    if numero % 4 == 0 {
+        println!("El numero es divisible entre 4");
+    } else if numero % 3 == 0 {
+        println!("El numero es divisible entre 3");
+    } else if numero % 2 == 0 {
+        println!("El numero es divisible entre 2");
     } else {
-        println!("La condicion es falsa!");
+        println!("el numero no es divisible ni por 4, 3, o 2");
     }
 }
