39ad31c72ffc1d3de7b14f7a64f9436db7ed5c18
Oscar Vazquez <0skr.vazquez@gmail.com>
2024-05-19 19:15:57 -0600

    delete target

--- target/debug/deps/estudio_rust-7ec3bb7e9d3bc955.d
+++
@@ -1,5 +0,0 @@
-/home/oscar/estudio-rust/target/debug/deps/estudio_rust-7ec3bb7e9d3bc955: src/main.rs
-
-/home/oscar/estudio-rust/target/debug/deps/estudio_rust-7ec3bb7e9d3bc955.d: src/main.rs
-
-src/main.rs:
--- target/debug/estudio_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/oscar/Documents/estudio-rust/target/debug/estudio_rust: /Users/oscar/Documents/estudio-rust/src/main.rs
