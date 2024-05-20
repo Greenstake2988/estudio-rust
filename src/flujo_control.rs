6bc4d1d3269308bab88ca6f002095ba1e273ac3b
Oscar Vazquez <0skr.vazquez@gmail.com>
2024-05-19 21:22:10 -0600

    Terminando flujo de control

--- src/flujo_control.rs
+++ src/flujo_control.rs
@@ -26,9 +26,54 @@ fn main() {
         contador += 1;
 
         if contador == 10 {
-            break 100;
+            break contador * 2 
         }
     };
 
     println!("El resultado de la iteracion es {resultado}");
+
+    let mut contador = 0;
+   
+    // Puedes ponerle una etiqueta a tu bucle
+    // para que pueda posteriormente usar break o continue
+    // con la etiqueta para poder decidir cual de lso bucles andidados
+    // deseas romper
+    'contador_arriba: loop {
+        println!("contador = {contador}");
+        let mut remanente = 10;
+        loop {
+            println!("remanente = {remanente}");
+            if remanente == 9 {
+                break;
+            }
+            if contador == 2 {
+                break 'contador_arriba;
+            }
+            remanente -= 1;
+        }
+
+    contador += 1;
+    }
+    println!("Final del contador = {contador}");
+
+    let mut numero = 3;
+    
+    while numero != 0 {
+        println!("{numero}!");
+
+        numero -= 1;
+    }
+    println!("LISTO!!");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for elemento in a {
+        println!("El valor es: {elemento}");
+    }
+
+    for numero in (1..4).rev() {
+        println!("{numero}!");
+    }
+    println!("LIFTOFF!!!");
 }
+
