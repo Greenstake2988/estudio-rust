3aaf685b002ac39cc29fa5d68f9a6cf330f321e7
Oscar Vazquez <0skr.vazquez@gmail.com>
2024-05-24 11:45:10 -0600

    colecciones

---
+++ src/colecciones.rs
@@ -0,0 +1,72 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let tercero: &i32 = &v[2];
+    println!("El tercer elemento es {tercero}");
+
+    let tercero = v.get(2);
+    match tercero {
+        None => println!("No existe el tercer elemento."),
+        Some(tercero) => println!("El tercer elemento es {tercero}")
+    }
+    let v = vec![1, 2, 3, 4, 5];
+    //let no_existe = &v[100];
+    let no_existe = v.get(100);
+
+    let v = vec![100, 32, 57];
+    for i in &v{
+        println!("{i}")
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+
+    enum HojaDeCalculo {
+        Entero(i32),
+        Flotante(f64),
+        Texto(String),
+    }
+
+    let fila = vec![
+        HojaDeCalculo::Entero(3),
+        HojaDeCalculo::Texto(String::from("azul")),
+        HojaDeCalculo::Flotante(10.12),
+    ];
+
+    let mut s = String::new();
+    s = "Hola".to_string();
+    s.push_str("Mundo!");
+    s.push('!');
+
+    println!("{s}");
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+
+    println!("{s3}");
+
+    let hola = "hola";
+    let h = &hola[0..1];
+    println!("{h}");
+
+    for c in "नमस्ते".chars() {
+        println!("{c}");
+    }
+
+    for b in "3д".bytes() {
+        println!("{b}");
+    }
+
+}
+
