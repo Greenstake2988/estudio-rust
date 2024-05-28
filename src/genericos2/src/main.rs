bd8088d026ec8ff40cd1620a520cf4eba3a31170
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2024-05-28 15:44:26 -0600

    no compila

--- src/genericos2/src/main.rs
+++ src/genericos2/src/main.rs
@@ -1,4 +1,4 @@
-fn el_mas_grande_i32(lista: &[i32]) -> &i32 {
+fn el_mas_grande<T>(lista: &[T]) -> &T {
     let mut el_mas_grande = &lista[0];
 
     for item in lista {
@@ -8,29 +8,16 @@ fn el_mas_grande_i32(lista: &[i32]) -> &i32 {
     }
 
     el_mas_grande
-    
 }
-
-fn el_mas_grande_char(lista: &[char]) -> &char {
-    let mut el_mas_grande = &lista[0];
-
-    for item in lista {
-        if item > el_mas_grande {
-            el_mas_grande = item;
-        }
-    }
-    el_mas_grande
-}
-
 
 fn main() {
     let lista_de_numeros = vec![34, 50, 25, 100, 65];
 
-    let resultado = el_mas_grande_i32(&lista_de_numeros);
+    let resultado = el_mas_grande(&lista_de_numeros);
     println!("El numero mas grande es {}", resultado);
 
     let lista_de_chars = vec!['y', 'm', 'a', 'q'];
 
-    let resultado = el_mas_grande_char(&lista_de_chars);
+    let resultado = el_mas_grande(&lista_de_chars);
     println!("El char mas grande es {}", resultado);
 }
